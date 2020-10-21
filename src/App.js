24c988ea9c2dea961eb2d9e47e720dad98af833a
subhampreet <subhampreet18@gmail.com>
2020-10-22 02:08:01 +0530

    header-initiated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <GitHubIcon fontSize="large" className="header-icon"/>
+        <span>Github</span> 
       </header>
     </div>
   );
