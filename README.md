a59f5098244b1c5e8d2028dc5d9603805a6cbf6c
subhampreet <subhampreet18@gmail.com>
2020-10-23 16:25:06 +0530

    About Page Added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Github Profile Finder Application
+# Github Profile Finder Application 🐱‍👤
+
+![0](https://user-images.githubusercontent.com/61475220/96919833-f701bb80-14c9-11eb-8eea-1c46ba92b906.jpg)
+
+<b>GitHub</b>, Inc. is an American multinational corporation that provides hosting for software development and version control using Git. It offers the distributed version control and source code management (SCM) functionality of Git, plus its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and continuous integration.
+
+<b>Github Profile Finder</b> is built using React JS. It simply searches the profile using the username and displays the profile details, along with the the repositories. It also labels the profile hireable or non-hireable according to contribution made and user activity. The app uses the github api to fetch data from github.
+
+### Features : 
+- Fetches all profiles with the provided username
+- Displays all the details of the selected profile
+- Labels the profile as hireable or non-hireable
+
+### Dependancies Installed : 
+- npm install react-router-dom
+- npm install react-bootstrap
+- npm install react-markdown
+- npm install @material-ui/core
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,26 @@ import React, { Fragment } from 'react';
 
 const About = () => {
   return (
-    <Fragment>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
-    </Fragment>
+    <div className="about">
+      <div className="card">
+       <img src="/0.jpg" />  
+       <div className="heading">About the App</div>
+      <p>
+      <b>GitHub</b>, Inc. is an American multinational corporation that provides hosting for software development and version control using Git. It offers the distributed version control and source code management (SCM) functionality of Git, plus its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, and continuous integration.
+      <br /> <br />
+      <b>Github Profile Finder</b> is built using React JS. It simply searches the profile using the username and displays the profile details, along with the the repositories. It also labels the profile hireable or non-hireable according to contribution made and user activity. The app uses the github api to fetch data from github.
+
+      </p>
+
+      <div className="heading">Features</div>
+      <ul>
+        <li>- Fetches all profiles with the provided username</li>
+        <li>- Displays all the details of the selected profile</li>
+        <li>- Labels the profile as hireable or non-hireable</li>
+      </ul>
+      
+      </div>
+    </div>
   );
 };
 
